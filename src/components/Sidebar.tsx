705b8e7bb4b37d03d77dfdf5bcfe0e36d63e650f
antran1245 <antran1245@gmail.com>
2023-01-07 13:24:05 -0800

    created a sidebar with desktop style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ReactComponent as Logo } from '../assets/logo.svg'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import '../sass/sidebar.scss'
+
 export default function Sidebar() {
   return (
-    <nav></nav>
+    <nav>
+      <div className='logo'>
+        <Logo />
+        <span></span>
+      </div>
+      <div className='avatarMode'>
+        <span className="mode">
+          <FontAwesomeIcon icon={faMoon} />
+        </span>
+        <span className='avatar'>
+          <img src={require('../assets/image-avatar.jpg')} alt="avatar" />
+        </span>
+      </div>
+    </nav>
   )
 }

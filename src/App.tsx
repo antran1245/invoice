b4e7f50f8837e24988d863887867cc0846bb97f4
antran1245 <antran1245@gmail.com>
2023-01-09 18:32:39 -0800

    constructed a context for selected invoice

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { Col, Container, Row } from 'react-bootstrap';
 import Sidebar from './components/Sidebar';
 import InvoicesListing from './components/InvoicesListing';
 import './App.css';
+import InvoiceContext from './context/InvoiceContext';
 
 function App() {
   const [theme, setTheme] = useState<boolean>(false)
   return (
-    <Container fluid className='m-0 p-0' data-color-mode={theme ? 'dark' : 'light'}>
-      <Sidebar theme={theme} setTheme={setTheme} />
-      <Container>
-        <Row>
-          <Col xs={12} lg={{ offset: 1, span: 8 }}>
-            <InvoicesListing />
-          </Col>
-        </Row>
+    <InvoiceContext>
+      <Container fluid className='m-0 p-0' data-color-mode={theme ? 'dark' : 'light'}>
+        <Sidebar theme={theme} setTheme={setTheme} />
+        <Container>
+          <Row>
+            <Col xs={12} lg={{ offset: 1, span: 8 }}>
+              <InvoicesListing />
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+    </InvoiceContext>
   );
 }
 
---
+++ src/context/InvoiceContext.tsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+import { InvoiceInterface } from "../interfaces/InvoiceInterface";
+
+interface ContextProps {
+  invoice: InvoiceInterface | null;
+  setInvoice: React.Dispatch<React.SetStateAction<InvoiceInterface | null>>;
+}
+export const Context = createContext<ContextProps | null>(null)
+
+export default function InvoiceContext({ children }: any) {
+  const [invoice, setInvoice] = useState<InvoiceInterface | null>(null)
+  return (
+    <Context.Provider value={{ invoice, setInvoice }}>
+      {children}
+    </Context.Provider>
+  )
+}
+

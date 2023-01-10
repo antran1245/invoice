c5d76fbb6d35bd6d5c619de7c85c23692ea017d8
antran1245 <antran1245@gmail.com>
2023-01-09 17:37:04 -0800

    function to filter the invoices list

--- src/components/InvoicesListing.tsx
+++ src/components/InvoicesListing.tsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Col, Dropdown, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faCirclePlus, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import Invoice from "./Invoice";
 import empty from '../assets/illustration-empty.svg'
-import data from '../data.json'
+import file from '../data.json'
 import "../sass/invoiceslisting.scss"
+import { InvoiceInterface } from "../interfaces/InvoiceInterface";
 
 export default function InvoicesListing() {
   const [angle, setAngle] = useState<boolean>(true)
+  const [data, setData] = useState<InvoiceInterface[] | []>([])
+  const [filter, setFilter] = useState<{ 'draft': boolean, 'pending': boolean, 'paid': boolean }>({ 'draft': false, 'pending': false, 'paid': false })
+
+  useEffect(() => {
+    setData(file)
+  }, [])
+
+  /**
+   * Reset all the filter to default (false).
+   * Filter the invoices by a status.
+   * Or show all invoices if no checkbox is checked.
+   * @param e : change in checkbox check or uncheck
+   */
+  const filterStatus = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      let curr = file.filter(item => item.status === e.target.value)
+      let defaultFilter = { 'draft': false, 'pending': false, 'paid': false }
+      setData(curr)
+      setFilter({ ...defaultFilter, [e.target.value]: true })
+    } else {
+      setData(file)
+      setFilter({ 'draft': false, 'pending': false, 'paid': false })
+    }
+  }
+
   return (
     <div id="invoicesListing">
       <Row>
@@ -23,9 +49,9 @@ export default function InvoicesListing() {
                 Filter by status <FontAwesomeIcon icon={angle ? faAngleDown : faAngleUp} />
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                <div><label><input type={"checkbox"} value="Draft" /> Draft</label></div>
-                <div><label><input type={"checkbox"} value="Pending" /> Pending</label></div>
-                <div><label><input type={"checkbox"} value="Paid" /> Paid</label></div>
+                <div><label><input type={"checkbox"} value="draft" onChange={(e) => filterStatus(e)} checked={filter.draft} /> Draft</label></div>
+                <div><label><input type={"checkbox"} value="pending" onChange={(e) => filterStatus(e)} checked={filter.pending} /> Pending</label></div>
+                <div><label><input type={"checkbox"} value="paid" onChange={(e) => filterStatus(e)} checked={filter.paid} /> Paid</label></div>
               </Dropdown.Menu>
             </Dropdown>
             <button className="new"><FontAwesomeIcon icon={faCirclePlus} /> New Invoice</button>

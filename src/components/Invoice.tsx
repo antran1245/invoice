0dd26610f62c0e96d6782d9e58e71bd705417be1
antran1245 <antran1245@gmail.com>
2023-01-13 11:27:21 -0800

    invoices listing style for mobile

--- src/components/Invoice.tsx
+++ src/components/Invoice.tsx
@@ -10,31 +10,34 @@ export default function Invoice(props: InvoiceInterface) {
   const context = useContext(Context)
   return (
     <Row className="invoice" onClick={() => context?.setInvoice(props)}>
-      <Col md={2}>
+      <Col xs={6} md={2}>
         <p className="id">
           <span>#</span>{props.id}
         </p>
       </Col>
-      <Col md={3}>
+      <Col md={2} className="d-none d-md-block">
         <p className="duedate">
           <span>Due</span> {props.paymentDue}
         </p>
       </Col>
-      <Col md={2}>
+      <Col xs={5} md={2} className="text-center">
         <p className="name">
           {props.clientName}
         </p>
       </Col>
-      <Col md={2}>
-        <p className="price">
+      <Col xs={4} md={2} className="mt-4 mt-md-0">
+        <p className="duedate d-md-none">
+          <span>Due</span> {props.paymentDue}
+        </p>
+        <p className="price text-start text-md-end mt-2 mt-md-0">
           <FontAwesomeIcon icon={faEuroSign} /> {props.total.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
         </p>
       </Col>
-      <Col md={2} className={`status ${props.status}`}>
+      <Col xs={5} md={2} className={`status mt-4 mt-md-0 ${props.status}`}>
         <span>&#x2022;</span>
         <p>{props.status}</p>
       </Col>
-      <Col className="arrow">
+      <Col xs={1} className="arrow mt-4 mt-md-0">
         <FontAwesomeIcon icon={faAngleRight} />
       </Col>
     </Row>

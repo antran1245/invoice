1bbcaf0fffaf2acc1daf75f21c7d1b175b6a010f
antran1245 <antran1245@gmail.com>
2023-01-17 23:05:09 -0800

    working on stylying the form for mobile

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -7,130 +7,132 @@ export default function InvoiceForm() {
   return (
     <Form>
       <p>Bill Form</p>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>Street Address</Form.Label>
-            <Form.Control type="text" placeholder="Street Address" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>City</Form.Label>
-            <Form.Control type="text" placeholder="City" />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group>
-            <Form.Label>Post Code</Form.Label>
-            <Form.Control type="text" placeholder="Post Code" />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group>
-            <Form.Label>Country</Form.Label>
-            <Form.Control type="text" placeholder="Country" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <p>Bill To</p>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>Client's Name</Form.Label>
-            <Form.Control type="text" placeholder="Name" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>Client's Email</Form.Label>
-            <Form.Control type="text" placeholder="Email" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>Street Address</Form.Label>
-            <Form.Control type="text" placeholder="Street Address" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>City</Form.Label>
-            <Form.Control type="text" placeholder="City" />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group>
-            <Form.Label>Post Code</Form.Label>
-            <Form.Control type="text" placeholder="Post Code" />
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group>
-            <Form.Label>Country</Form.Label>
-            <Form.Control type="text" placeholder="Country" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>Invoice Date</Form.Label>
-            <div className="calendar">
-              <Form.Control type="date" placeholder="City" />
-              <FontAwesomeIcon icon={faCalendar} />
-            </div>
-          </Form.Group>
-        </Col>
-        <Col>
-          <Form.Group>
-            <Form.Label>Payment Terms</Form.Label>
-            <Dropdown id="payment-terms" className="h-100">
-              <Dropdown.Toggle className="w-100">
-                Net 30 Days <FontAwesomeIcon icon={faAngleDown} />
-              </Dropdown.Toggle>
-              <Dropdown.Menu className="w-100">
-                <div>Net 1 Day</div>
-                <Dropdown.Divider />
-                <div>Net 7 Day</div>
-                <Dropdown.Divider />
-                <div>Net 14 Day</div>
-                <Dropdown.Divider />
-                <div>Net 30 Day</div>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form.Group>
-            <Form.Label>Project Description</Form.Label>
-            <Form.Control type="text" placeholder="Project Description" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <h2>Item List</h2>
-      <Row className="heading">
-        <Col>Item Name</Col>
-        <Col>Qty.</Col>
-        <Col>Price</Col>
-        <Col>Total</Col>
-      </Row>
-      <Row>
-        <Col>
-          <button className="w-100 add-button" >+Add New Item</button>
-        </Col>
-      </Row>
-      <Row className="justify-content-between">
+      <div>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Street Address</Form.Label>
+              <Form.Control type="text" placeholder="Street Address" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control type="text" placeholder="City" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>Post Code</Form.Label>
+              <Form.Control type="text" placeholder="Post Code" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>Country</Form.Label>
+              <Form.Control type="text" placeholder="Country" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <p>Bill To</p>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Client's Name</Form.Label>
+              <Form.Control type="text" placeholder="Name" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Client's Email</Form.Label>
+              <Form.Control type="text" placeholder="Email" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Street Address</Form.Label>
+              <Form.Control type="text" placeholder="Street Address" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control type="text" placeholder="City" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>Post Code</Form.Label>
+              <Form.Control type="text" placeholder="Post Code" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>Country</Form.Label>
+              <Form.Control type="text" placeholder="Country" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Invoice Date</Form.Label>
+              <div className="calendar">
+                <Form.Control type="date" placeholder="City" />
+                <FontAwesomeIcon icon={faCalendar} />
+              </div>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>Payment Terms</Form.Label>
+              <Dropdown id="payment-terms" className="h-100">
+                <Dropdown.Toggle className="w-100">
+                  Net 30 Days <FontAwesomeIcon icon={faAngleDown} />
+                </Dropdown.Toggle>
+                <Dropdown.Menu className="w-100">
+                  <div>Net 1 Day</div>
+                  <Dropdown.Divider />
+                  <div>Net 7 Day</div>
+                  <Dropdown.Divider />
+                  <div>Net 14 Day</div>
+                  <Dropdown.Divider />
+                  <div>Net 30 Day</div>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Project Description</Form.Label>
+              <Form.Control type="text" placeholder="Project Description" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <h2>Item List</h2>
+        <Row className="heading">
+          <Col>Item Name</Col>
+          <Col>Qty.</Col>
+          <Col>Price</Col>
+          <Col>Total</Col>
+        </Row>
+        <Row>
+          <Col>
+            <button className="w-100 add-button" >+Add New Item</button>
+          </Col>
+        </Row>
+      </div>
+      <Row id="actionContainer" className="justify-content-between">
         <Col md={2}>
           <button className="discard">Discard</button>
         </Col>
